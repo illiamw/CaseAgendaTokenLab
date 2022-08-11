83ce2345cc1547c12a585b7ff59292f8d070234d
William Ferreira <33575357+illiamw@users.noreply.github.com>
2022-08-11 06:16:44 -0300

    huroku

--- AgendaFront/src/app/app.module.ts
+++ AgendaFront/src/app/app.module.ts
@@ -11,11 +11,6 @@ import { AuthInterceptorService } from './guard/auth-interceptor.service';
 import { RectrictedModule } from './restricted/rectricted.module';
 
 
-// Agenda Gabriel Missali: https://calendly.com/gabrielmissali/entrevista-tecnica-de-web
-//   Agenda Guilherme Sigoli: https://calendly.com/guis/entrevista-e
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
---
+++ postgres-dev-init.sql
@@ -0,0 +1,3 @@
+CREATE USER adonis with encrypted password 'adonis';
+CREATE DATABASE Agenda;
+GRANT ALL PRIVILEGES ON DATABASE adonis_app TO adonis;
